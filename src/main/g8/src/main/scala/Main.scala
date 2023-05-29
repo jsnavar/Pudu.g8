a31019d9fb1f89e83a479abe1bed9a9db80da2d4
Juan Navarro <juansnn@pm.me>
2023-05-29 00:11:25 +0000

    Avoid clash on name "pudu"

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,18 +1,17 @@
 @main def hello: Unit =
+  /* https://www.asciiart.eu/animals/deer */
+  val pudu = """
+           /',  _
+         _(  ;-'.'
+    _,-'~     '".
+  '"          ~. .
+          _.      '.
+      _.-'  ~'--.   )
+    '~           ~--'=._
+                 /)_.-.
+             _.-' ' <~
+          .-"    _ ~ \
+              .-' '-._)
+            .'                     PjP"""
+
   println(pudu)
-
-/* https://www.asciiart.eu/animals/deer */
-val pudu = """
-         /',  _
-       _(  ;-'.'
-  _,-'~     '".
-'"          ~. .
-        _.      '. 
-    _.-'  ~'--.   ) 
-  '~           ~--'=._
-               /)_.-.
-           _.-' ' <~
-        .-"    _ ~ \
-            .-' '-._)
-          .'                     PjP"""
-
